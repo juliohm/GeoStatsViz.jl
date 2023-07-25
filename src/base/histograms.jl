f0b7b7e9d7c3b58239ae1e90897f03751b9c2526
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-07-25 14:43:41 -0300

    Refactor histogram recipe

--- src/base/histograms.jl
+++ src/base/histograms.jl
@@ -2,21 +2,7 @@
 # Licensed under the MIT License. See LICENCE in the project root.
 # ------------------------------------------------------------------
 
-Makie.plottype(::EmpiricalHistogram) = Viz{<:Tuple{EmpiricalHistogram}}
+Makie.plottype(::EmpiricalHistogram) = Makie.Hist
 
-function Makie.plot!(plot::Viz{<:Tuple{EmpiricalHistogram}})
-  # retrieve histogram object
-  hist = plot[:object]
-
-  # abscissa and ordinates
-  coords = Makie.@lift values($hist)
-  xs = Makie.@lift $coords[1]
-  ys = Makie.@lift $coords[2]
-
-  Makie.barplot!(plot, xs, ys,
-    color       = plot[:color],
-    strokecolor = plot[:facetcolor],
-    strokewidth = 0.1,
-    gap         = 0.0,
-  )
-end
+Makie.convert_arguments(P::Type{<:Makie.Hist}, h::EmpiricalHistogram) =
+  Makie.convert_arguments(P, h.hist)
