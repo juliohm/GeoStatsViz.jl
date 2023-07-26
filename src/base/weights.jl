473e896cd702e4cdd4482a67032d2bb574b74dfb
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-07-25 22:16:46 -0300

    More refactoring

--- src/base/weights.jl
+++ src/base/weights.jl
@@ -2,23 +2,9 @@
 # Licensed under the MIT License. See LICENCE in the project root.
 # ------------------------------------------------------------------
 
-Makie.plottype(::GeoWeights) = Viz{<:Tuple{GeoWeights}}
+Makie.plottype(::GeoWeights) = Viz
 
-function Makie.plot!(plot::Viz{<:Tuple{GeoWeights}})
-  # retrieve weights object
-  weights = plot[:object]
-
-  # underlying domain and values
-  wdomain = Makie.@lift domain($weights)
-  wvalues = Makie.@lift collect($weights)
-
-  viz!(plot, wdomain,
-    color       = wvalues,
-    alpha       = plot[:alpha],
-    colorscheme = plot[:colorscheme],
-    facetcolor  = plot[:facetcolor],
-    showfacets  = plot[:showfacets],
-    pointsize   = plot[:pointsize],
-    segmentsize = plot[:segmentsize]
-  )
+function Makie.convert_arguments(P::Type{<:Viz}, w::GeoWeights) 
+  dom = Makie.convert_arguments(P, domain(w))
+  Makie.PlotSpec{Viz}(dom...; color = collect(w))
 end
